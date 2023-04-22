7eb38d3fb25bce6d8f641c611b30666a85d8b54a
Gloria <gloria.wan.glory@gmail.com>
2023-04-22 16:16:53 -0400

    changed spacing for readability

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 class Planet:
     def __init__(self, id, name, description):
@@ -7,27 +7,24 @@ class Planet:
         self.description = description
 
 #create a list of Planets
-
 planets = [
     Planet (1, "Mars", "We're still looking for water here"),
     Planet(9, "Saturn", "Maybe it's kind of yellow"),
     Planet (5, "Pluto", "Basically the forgotten middle child of Planets")
 ]
 
+# creates planet blueprint
 planets_bp = Blueprint("books", __name__, url_prefix="/planets")
 
 @planets_bp.route("", methods=["GET"])
 
 #define the function to handle the planets
 def handle_planets():
-
     planets_response = []
-
     for planet in planets:
         planets_response.append({
             "id": planet.id,
             "name": planet.name,
             "description": planet.description
         })
-
     return jsonify(planets_response)
