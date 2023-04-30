4e5104718a56ef0cc9f970ecbb1653c702c2a3d6
Kaliane <vankaliane@gmail.com>
2023-04-29 22:16:03 -0400

    Changed 'moon' data type from 'db.Boolean' to 'db.Integer' in the 'planet.py' file and also in the psql using the ALTER TABLE and ALTER COLUMN commmands. Both 'handle_planet' and 'read_all_planet' fuctions are now working. Added code straight from learn lesson '(04) Building an API - Read, Update, And Delete') to complete wave 4, including the following functions: 'validate_planet' to handle 404 and 400 errors, 'read_one_planet' to get a single planet, 'update_planet' to update a single planet, and 'delete_planet' to delete a single planet.

--- app/routes.py
+++ app/routes.py
@@ -59,13 +59,63 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 #     }
 
 
+
+@planets_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(f"planet #{planet.id} successfully deleted")
+
+
+@planets_bp.route("/<planet_id>", methods=["PUT"])
+
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.moons = request_body["moons"]
+
+    db.session.commit()
+
+    return make_response(f"Planet #{planet.id} successfully updated")
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    planet = Planet.query.get(planet_id)
+
+    if not planet:
+        abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+
+    return planet
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+
+def read_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "moons": planet.moons
+        }
+    
+
 @planets_bp.route("", methods=['POST'])
 
 def handle_planet():
     request_body = request.get_json()
     
     new_planet = Planet(
-        id = request_body['id'],
         name = request_body['name'],
         description = request_body['description'],
         moons = request_body['moons']
@@ -87,7 +137,7 @@ def read_all_planets():
             "id": planet.id,
             "name": planet.name,
             "description": planet.description,
-            "moon": planet.moons
+            "moons": planet.moons
         })
         
     return jsonify(planets_response)
